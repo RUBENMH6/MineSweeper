0f9f241097a6cda18ab206197e03070e42bdd40a
alu10211999 <alu10211999@ti3-09>
2023-03-06 16:54:25 +0100

    First commit

---
+++ src/main/java/com/mycompany/minesweeper/Button.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.minesweeper;
+
+import javax.swing.JButton;
+
+/**
+ *
+ * @author alu10211999
+ */
+public class Button extends JButton {
+    private CellState state;
+}
